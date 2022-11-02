601ac09956630663dd622de93305de89e4547ebe
zenoyang <zenoyang@apache.org>
2022-11-03 03:18:07 +0800

    [dev] try avx2 & and vectorclass

--- tests/zeno_dev_array_bitset.cpp
+++ tests/zeno_dev_array_bitset.cpp
@@ -6,36 +6,54 @@
 #include "containers/bitset.h"
 #ifdef __AVX2__
 #include <immintrin.h>  // avx2
-#include <nmmintrin.h>  // sse4.2
-#include <smmintrin.h>  // sse4.1
-#include <tmmintrin.h>  // sse3
-#include <pmmintrin.h>  // sse3
-#include <emmintrin.h>  // sse2
-#include <xmmintrin.h>  // sse
-#include <mmintrin.h>   // mxx
-
-#include <avx_mathfun>
+//#include <nmmintrin.h>  // sse4.2
+//#include <smmintrin.h>  // sse4.1
+//#include <tmmintrin.h>  // sse3
+//#include <pmmintrin.h>  // sse3
+//#include <emmintrin.h>  // sse2
+//#include <xmmintrin.h>  // sse
+//#include <mmintrin.h>   // mxx
+//#include <avx_mathfun>
 #endif
 
-// todo(zeno) need simd
+#include "vectorclass/vectori256.h"
+
+
 /*
- * 1. AVX2没有_mm256_div_epi16指令，所以将uint16转为uint32_t进行处理，一次处理256/32=8个数据
- * 2. 没找到AVX2取余指令，通过与或运算得到取余结果
+ * todo(zeno) need simd
+ * // 1. AVX2没有_mm256_div_epi16指令，所以将uint16转为uint32_t进行处理，一次处理256/32=8个数据
+ * // 2. 没找到AVX2取余指令，通过与或运算得到取余结果
+
+// __m128i pos16 = _mm_lddqu_si128((const __m128i *)(list + i));
+// __m256i pos32 = _mm256_cvtepi16_epi32(pos16);
+
+// Vector of 16 16-bit unsigned integers
+// Vec16us pos;
+// pos.load(list + i);
+// Vec16us offset = pos >> 6;
+// pos % 64;
+// pos & 1;
  */
 static inline void _avx2_bitset_set_list(uint64_t *words, const uint16_t *list, uint64_t length) {
     uint64_t offset, load, newload, pos, index;
 
-    for (uint64_t i = 0; i < length; i += 8) { // 256/32 -> 16
-        __m256i A = _mm256_lddqu_si256((const __m256i *)(list + i));    // ymm 16 uint16_t
-        // set 64 ymm
-        __m256i div_mod_64_vec = _mm256_set1_epi16(64);
-//        __m256i offset = _mm256_div_epi16(A, div_mod_64_vec);
-        __m256i offset = _mm256_div_pd(A, div_mod_64_vec);
+    // todo first 一次只处理4个list元素
+    for (uint64_t i = 0; i < length; i += 16) { // 256/16 -> 16
+         __m256i pos_vec = _mm256_lddqu_si256((const __m256i *)(list + i));    // 16 16-bit unsigned integers
+         __m256i offset_vec16 = _mm256_srli_epi16(pos_vec, 6);
+         __m256i vec63 = _mm256_set1_epi16(63);
+         __m256i index_vec16 = _mm256_and_si256(pos_vec, vec63);  // mod 64
+
+         __m256i offset_vec32 = offset_vec16;   // todo cvt ... offset_vec16拆成4个offset_vec，每个offset_set有4个offset
+         __m256i load_vec = _mm256_i32gather_epi32(words, offset_vec32, 8); // 4 word
+         __m256i index_vec64 = index_vec16; // todo index_vec16拆成4个index_vec64, 每个index_vec64有4个index
+         __m256i vec64_1 = _mm256_set1_epi64x(1ull);
+         // __m256i tmp = _mm256_sll_epi64(vec64_1, index_vec64);
 
 
     }
 
-    // 尾部处理
+    // todo 尾部处理
 }
 
 static inline void _scalar_bitset_set_list(uint64_t *words, const uint16_t *list, uint64_t length) {
